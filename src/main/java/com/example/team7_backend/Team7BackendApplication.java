77816543eae913a20a871559e77f6f09ca0198d2
Jack Curtin <jackcurtin94@gmail.com>
2021-05-07 13:13:05 -0500

    first commit

---
+++ src/main/java/com/example/team7_backend/Team7BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.team7_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team7BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team7BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/team7_backend/Team7BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.team7_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team7BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
